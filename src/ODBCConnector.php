6d626b946e9b400667a6cbe3fb7dbf01e74e1c71
Olly Butterfield <olly@opb.me.uk>
2015-06-22 08:41:02 +0100

    Start adding files

---
+++ src/ODBCConnector.php
@@ -0,0 +1,47 @@
+<?php namespace Opb\LaravelOdbcDb2;
+
+use Illuminate\Database\Connectors\Connector;
+use Illuminate\Database\Connectors\ConnectorInterface;
+
+class ODBCConnector extends Connector implements ConnectorInterface
+{
+    /**
+     * Establish a database connection.
+     *
+     * @param  array  $config
+     * @return \PDO
+     */
+    public function connect(array $config)
+    {
+        $dsn = $this->getDsn($config);
+
+        $options = $this->getOptions($config);
+
+        $connection = $this->createConnection($dsn, $config, $options);
+
+        if (isset($config['schema']))
+        {
+            $schema = $config['schema'];
+
+            $connection->prepare("set schema $schema")->execute();
+        }
+
+        return $connection;
+    }
+
+    /**
+     * Get the DSN string for a host / port configuration.
+     *
+     * @param  array  $config
+     * @return string
+     */
+    protected function getDsn(array $config)
+    {
+        extract($config);
+
+        $port = isset($port) ? $port : 50000;
+
+        return "odbc:DRIVER={".$odbc_driver."};DATABASE=$database;HOSTNAME=$host;PORT=$port;PROTOCOL=TCPIP;UID=$username;PWD=$password;";
+    }
+}
+
